b617e97e2f101ef3ad1dfe1203350d157cf78112
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-17 16:18:48 -0600

    Create getPlayers() in App component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,46 @@ import TeamStats from './Team-stats';
 import Player from './Player';
 
 export default class App extends Component{
+  getPlayers() {
+    return [
+      {
+        _id: 1,
+        name: "Oliver Alexander"
+        dribbling: 2,
+        shooting: 3,
+        passing: 1,
+        tackling: 1,
+        speed: 3,
+        blocking: 2.
+        strategy: 3,
+        playmaking: 2
+      },
+      {
+        _id: 2,
+        name: "Lisa Tarbuck"
+        dribbling: 1,
+        shooting: 3,
+        passing: 2,
+        tackling: 3,
+        speed: 1,
+        blocking: 1.
+        strategy: 3,
+        playmaking: 2
+      },
+      {
+        _id: 3,
+        name: "Johnathon Ross"
+        dribbling: 1,
+        shooting: 3,
+        passing: 3,
+        tackling: 1,
+        speed: 2,
+        blocking: 2.
+        strategy: 2,
+        playmaking: 2
+      },
+    ]
+  }
   render() {
     return (
       <MuiThemeProvider>
