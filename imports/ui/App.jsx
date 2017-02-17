6597976946bd042d11d5227b0c0007d0f137fa3c
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-17 15:28:57 -0600

    Import components

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
+import TeamList from './Team-list';
+import TeamStats from './Team-stats';
+import Player from './Player';
+
 export default class App extends Component{
   render() {
     return (
       <MuiThemeProvider>
-        <RaisedButton label="Boom! We did it!" />
+        <div><Player /></div>
+        <div><TeamStats /></div>
+        <div><TeamList /></div>
       </MuiThemeProvider>
     )
   }
