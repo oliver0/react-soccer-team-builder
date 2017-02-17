c1412693c754b95370e6bb977fbbdd9c0e37d1f4
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-17 15:50:40 -0600

    Add img. Modify .gitignore

--- imports/ui/Player.jsx
+++ imports/ui/Player.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { Card, CardMedia, CardText, CardActions, CardTitle} from 'material-ui/Card';
 
 
 export default class Player extends Component{
   render() {
     return (
-      <h1>Player List</h1>
-    )
+      <Card>
+        <CardMedia
+          overlay={<CardTitle title="Oliver Alexander" subtitle="Offense: 12 - Defence:8" />}
+          >
+            <img src="player.jpg" />
+          </CardMedia>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardActions>
+          </CardActions>
+        </Card>
+      )
+    }
   }
-}
