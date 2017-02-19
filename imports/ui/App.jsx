56c88b793483d57f551b454ae2961a074c6eba5c
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-19 15:04:51 -0600

    Import List, Divider. Add renderPlayers(). Modify render()

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
+import { List } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 
 import TeamList from './Team-list';
 import TeamStats from './Team-stats';
@@ -12,42 +14,49 @@ export default class App extends Component{
     return [
       {
         _id: 1,
-        name: "Oliver Alexander"
+        name: "Oliver Alexander",
         dribbling: 2,
         shooting: 3,
         passing: 1,
         tackling: 1,
         speed: 3,
-        blocking: 2.
+        blocking: 2,
         strategy: 3,
         playmaking: 2
       },
       {
         _id: 2,
-        name: "Lisa Tarbuck"
+        name: "Lisa Tarbuck",
         dribbling: 1,
         shooting: 3,
         passing: 2,
         tackling: 3,
         speed: 1,
-        blocking: 1.
+        blocking: 1,
         strategy: 3,
         playmaking: 2
       },
       {
         _id: 3,
-        name: "Johnathon Ross"
+        name: "Johnathon Ross",
         dribbling: 1,
         shooting: 3,
         passing: 3,
         tackling: 1,
         speed: 2,
-        blocking: 2.
+        blocking: 2,
         strategy: 2,
         playmaking: 2
       },
     ]
   }
+
+  renderPlayers() {
+      return this.getPlayers().map((player) => (
+        <TeamList key={player.id} player={player} />
+      ));
+  }
+
   render() {
     return (
       <MuiThemeProvider>
@@ -58,8 +67,14 @@ export default class App extends Component{
             showMenuIconButton={false}/>
           <div className="row">
             <div className="col s12 m7"><Player /></div>
+            <div className="col s12 m5">
+              <Divider />
+                <List>
+                  {this.renderPlayers()}
+                </List>
+              <Divider />
+            </div>
             <div className="col s12 m5"><TeamStats /></div>
-            <div className="col s12 m5"><TeamList /></div>
           </div>
         </div>
 
