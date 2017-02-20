5cbecf6dca2d3ed196d47b0172706992029b6810
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-19 22:10:29 -0600

    Add meteor package. Import Mongo. Create + attach PlayerSchema

---
+++ imports/api/players.js
@@ -0,0 +1,20 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Players = new Mongo.Collection('players');
+
+const PlayerSchema = new SimpleSchema({
+  team: { type: String},
+  name: { type: String},
+  dribbling: { type: Number, defaultValue: 0},
+  shooting: { type: Number, defaultValue: 0},
+  passing: { type: Number, defaultValue: 0},
+  tackling: { type: Number, defaultValue: 0},
+  speed: { type: Number, defaultValue: 0},
+  blocking: { type: Number, defaultValue: 0},
+  strategy: { type: Number, defaultValue: 0},
+  playmaking: { type: Number, defaultValue: 0},
+  notes: { type: String, optional: true},
+
+})
+
+Players.attachSchema(PlayerSchema);
