0b9688ec15bea0e7ec93e05218cda91f9c8a3a8e
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-19 22:29:45 -0600

    Add remaining form fields

--- imports/ui/New.jsx
+++ imports/ui/New.jsx
@@ -12,12 +12,13 @@ export default class Example extends Component{
               <input placeholder="Name" ref="name" type="text" className="validate"/>
             </div>
             <div className="input-field col s6">
-              <input placeholder="Name" ref="team" type="text" className="validate"/>
+              <input placeholder="Team" ref="team" type="text" className="validate"/>
             </div>
           </div>
 
           <div className="row">
             <div className="input-field col s6">
+              <h5>Dribbling</h5>
               <select className="browser-default" ref="dribbling">
                 <option value="0">0 - Hasn't demonstrated skills</option>
                 <option value="1">1 - Needs improvemnet</option>
@@ -26,7 +27,71 @@ export default class Example extends Component{
               </select>
             </div>
             <div className="input-field col s6">
+              <h5>Shooting</h5>
               <select className="browser-default" ref="shooting">
+                <option value="0">0 - Hasn't demonstrated skills</option>
+                <option value="1">1 - Needs improvemnet</option>
+                <option value="2">2 - Skill acquired</option>
+                <option value="3">3 - Great skills!</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s6">
+              <h5>Passing</h5>
+              <select className="browser-default" ref="passing">
+                <option value="0">0 - Hasn't demonstrated skills</option>
+                <option value="1">1 - Needs improvemnet</option>
+                <option value="2">2 - Skill acquired</option>
+                <option value="3">3 - Great skills!</option>
+              </select>
+            </div>
+            <div className="input-field col s6">
+              <h5>Tackling</h5>
+              <select className="browser-default" ref="tackling">
+                <option value="0">0 - Hasn't demonstrated skills</option>
+                <option value="1">1 - Needs improvemnet</option>
+                <option value="2">2 - Skill acquired</option>
+                <option value="3">3 - Great skills!</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s6">
+              <h5>Speed</h5>
+              <select className="browser-default" ref="speed">
+                <option value="0">0 - Hasn't demonstrated skills</option>
+                <option value="1">1 - Needs improvemnet</option>
+                <option value="2">2 - Skill acquired</option>
+                <option value="3">3 - Great skills!</option>
+              </select>
+            </div>
+            <div className="input-field col s6">
+              <h5>Blocking</h5>
+              <select className="browser-default" ref="blocking">
+                <option value="0">0 - Hasn't demonstrated skills</option>
+                <option value="1">1 - Needs improvemnet</option>
+                <option value="2">2 - Skill acquired</option>
+                <option value="3">3 - Great skills!</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s6">
+              <h5>Strategy</h5>
+              <select className="browser-default" ref="strategy">
+                <option value="0">0 - Hasn't demonstrated skills</option>
+                <option value="1">1 - Needs improvemnet</option>
+                <option value="2">2 - Skill acquired</option>
+                <option value="3">3 - Great skills!</option>
+              </select>
+            </div>
+            <div className="input-field col s6">
+              <h5>Playmaking</h5>
+              <select className="browser-default" ref="playmaking">
                 <option value="0">0 - Hasn't demonstrated skills</option>
                 <option value="1">1 - Needs improvemnet</option>
                 <option value="2">2 - Skill acquired</option>
