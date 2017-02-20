660835229dcf0a22fa208804ef66752aa53aa053
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-19 21:52:21 -0600

    Add files. Install yarn. Import files

--- client/main.jsx
+++ client/main.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from '../imports/ui/App.jsx';
+import Example from '../imports/ui/Example';
+import Lost from '../imports/ui/Lost'
 
 injectTapEventPlugin();
 
